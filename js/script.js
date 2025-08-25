8a10ea6f5935cc72b42d05e0939d6a269f8b1266
JODRAFF <traoresierachid@gmail.com>
2025-08-25 04:17:07 +0000

    i

--- js/script.js
+++ js/script.js
@@ -8,3 +8,32 @@ navLinks.forEach(link => {
     }
 });
 
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+// Affiche le bouton après avoir scrollé 200px
+window.onscroll = function() {
+    if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+};
+
+// Remonte en haut au clic
+scrollBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("visible");
+        }
+    });
+}, { threshold: 0.2 });
+
+sections.forEach(section => {
+    observer.observe(section);
+});
